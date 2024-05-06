4ed2d69fa10f87b9ad36b0afe8f29453e164d614
Julien Thomas <juthomas@tandemdiabetes.com>
2024-05-06 08:56:38 +0200

    [lesson02] LogAnalysis.hs: Errata, fix missing case from Leaf to simple Node

--- lesson02/LogAnalysis.hs
+++ lesson02/LogAnalysis.hs
@@ -21,4 +21,11 @@ insert :: LogMessage -> MessageTree -> MessageTree
 insert logMsg@(LogMessage _ ts1 _) (Node left node@(LogMessage _ ts2 _) right)
         | ts1 > ts2 = Node left node (insert logMsg right)
         | otherwise = Node (insert logMsg left) node right
+insert logMsg Leaf = Node Leaf logMsg Leaf
 insert _ tree = tree
+
+-- Exercise 3
+--
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x $ build xs
